5f0a3ca2cbb90068b6d483fc85430107ec8164fc
김진근 <freedom7113@gmail.com>
2022-03-29 14:11:42 +0900

    ⚙️ (#1) - setting provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,19 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { ThemeProvider } from "@emotion/react";
+import { theme } from "../src/utils/theme/theme";
+import { QueryClient, QueryClientProvider } from "react-query";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const queryClient = new QueryClient();
+
+  return (
+    <ThemeProvider theme={theme}>
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
+      </QueryClientProvider>
+    </ThemeProvider>
+  );
 }
 
 export default MyApp;
