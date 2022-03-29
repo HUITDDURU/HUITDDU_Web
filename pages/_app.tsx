3eca9f5868e264159a895b5e7bc3bf00a5e69784
김진근 <freedom7113@gmail.com>
2022-03-29 14:25:26 +0900

    ⚙️ (#1) - add toaster component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import type { AppProps } from "next/app";
 import { ThemeProvider } from "@emotion/react";
 import { theme } from "../src/utils/theme/theme";
 import { QueryClient, QueryClientProvider } from "react-query";
+import { Toaster } from "react-hot-toast";
 
 function MyApp({ Component, pageProps }: AppProps) {
   const queryClient = new QueryClient();
@@ -11,6 +12,7 @@ function MyApp({ Component, pageProps }: AppProps) {
     <ThemeProvider theme={theme}>
       <QueryClientProvider client={queryClient}>
         <Component {...pageProps} />
+        <Toaster position="top-center" />
       </QueryClientProvider>
     </ThemeProvider>
   );
