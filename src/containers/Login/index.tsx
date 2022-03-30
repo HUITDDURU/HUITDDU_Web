a4f77bf14f4086d2265c21e28ef6b716d00c33b0
김진근 <freedom7113@gmail.com>
2022-03-30 16:05:02 +0900

    🔌 (#3) - add inputs in login

--- src/containers/Login/index.tsx
+++ src/containers/Login/index.tsx
@@ -1,7 +1,23 @@
 import { NextPage } from "next";
+import Input from "../../components/Input";
+import * as S from "./styles";
 
 const LoginContainer: NextPage = () => {
-  return <div>this is login</div>;
+  return (
+    <div>
+      <S.Title>로그인</S.Title>
+      <S.InputContainer>
+        <div>
+          <S.Description>이메일</S.Description>
+          <Input placeholder="이메일을 입력해주세요..." />
+        </div>
+        <div>
+          <S.Description>비밀번호</S.Description>
+          <Input placeholder="비밀번호를 입력해주세요..." />
+        </div>
+      </S.InputContainer>
+    </div>
+  );
 };
 
 export default LoginContainer;
--- src/containers/Login/styles.ts
+++ src/containers/Login/styles.ts
@@ -1,1 +1,19 @@
-export {};
+import styled from "@emotion/styled";
+
+export const Title = styled.div`
+  font: ${({ theme }) => theme.fonts.heading2};
+  color: ${({ theme }) => theme.colors.grayscale.black};
+  margin-bottom: 28px;
+`;
+
+export const InputContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  row-gap: 18px;
+`;
+
+export const Description = styled.div`
+  font: ${({ theme }) => theme.fonts.description};
+  color: ${({ theme }) => theme.colors.grayscale.gray1};
+  margin-bottom: 8px;
+`;
