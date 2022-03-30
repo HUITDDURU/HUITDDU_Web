2ec8d83f05229999c22bf4746231790c975558ca
김진근 <freedom7113@gmail.com>
2022-03-30 15:06:39 +0900

    🔌 (#3) - add default container in login

--- src/containers/Login/index.tsx
+++ src/containers/Login/index.tsx
@@ -1,7 +1,24 @@
 import { NextPage } from "next";
+import * as S from "./styles";
+import LogoImage from "../../assets/images/login-logo.png";
+import Image from "next/image";
 
 const LoginContainer: NextPage = () => {
-  return <div>this is login</div>;
+  return (
+    <S.Container>
+      <S.LoginContainer>
+        <S.LogoContainer>
+          <Image
+            height="120px"
+            width="342px"
+            objectFit="contain"
+            src={LogoImage}
+            alt="logo"
+          />
+        </S.LogoContainer>
+      </S.LoginContainer>
+    </S.Container>
+  );
 };
 
 export default LoginContainer;
--- src/containers/Login/styles.ts
+++ src/containers/Login/styles.ts
@@ -3,4 +3,19 @@ import styled from "@emotion/styled";
 export const Container = styled.div`
   width: 100vw;
   height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const LoginContainer = styled.div`
+  padding: 36px;
+  border: solid 1px ${({ theme }) => theme.colors.grayscale.lightGray1};
+  border-radius: 10px;
+`;
+
+export const LogoContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-bottom: 36px;
 `;
