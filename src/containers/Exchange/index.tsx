1bb749c026eba170dea1e68d50b7ce0edc547981
김진근 <freedom7113@gmail.com>
2022-04-20 15:26:11 +0900

    🔌 (#8) - publishing select button ui

--- src/containers/Exchange/index.tsx
+++ src/containers/Exchange/index.tsx
@@ -1,8 +1,41 @@
 import { NextPage } from "next";
 import * as S from "./styles";
+import RandomUser from "../../assets/images/random-user.svg";
+import Friend from "../../assets/images/friend.svg";
+import Image from "next/image";
 
 const ExchangeContainer: NextPage = () => {
-  return <div>hello world</div>;
+  return (
+    <S.Container>
+      <S.Title>일기 교환 시작하기</S.Title>
+      <S.ButtonContainer>
+        <S.ButtonWrapper>
+          <S.Button>
+            <S.ButtonInner>
+              <Image src={RandomUser} alt="random user" />
+            </S.ButtonInner>
+            <S.Name>무작위 사용자 찾아보기</S.Name>
+            <S.Description>
+              무작위 사용자를 찾아봐요.
+              <br />
+              교환 수락을 하게되면 일기 교환이 시작돼요.
+            </S.Description>
+          </S.Button>
+          <S.Button>
+            <S.ButtonInner>
+              <Image src={Friend} alt="friend" />
+            </S.ButtonInner>
+            <S.Name>친구와 교환하기</S.Name>
+            <S.Description>
+              주어진 코드를 공유해
+              <br />
+              친구과 일기 교환을 할 수 있어요.
+            </S.Description>
+          </S.Button>
+        </S.ButtonWrapper>
+      </S.ButtonContainer>
+    </S.Container>
+  );
 };
 
 export default ExchangeContainer;
--- src/containers/Exchange/styles.ts
+++ src/containers/Exchange/styles.ts
@@ -1,8 +1,61 @@
 import styled from "@emotion/styled";
 
-export const Container = styled.div`
+export const Container = styled.main`
+  flex: 1;
   display: flex;
+  justify-content: center;
+  align-items: center;
   flex-direction: column;
-  min-height: 100vh;
-  justify-content: space-between;
+`;
+
+export const Title = styled.h1`
+  font: ${({ theme }) => theme.fonts.heading1};
+  color: ${({ theme }) => theme.colors.grayscale.black};
+  margin-bottom: 36px;
+`;
+
+export const ButtonContainer = styled.div`
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(6, 1fr);
+  column-gap: 20px;
+`;
+
+export const ButtonWrapper = styled.div`
+  grid-column: 2 / 6;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  column-gap: 20px;
+`;
+
+export const Button = styled.button`
+  border-radius: 10px;
+  border: 1px solid ${({ theme }) => theme.colors.grayscale.lightGray1};
+  height: 440px;
+  background-color: transparent;
+  transition: background-color 0.2s ease-in-out;
+  will-change: background-color;
+
+  &:hover {
+    background-color: ${({ theme }) => theme.colors.grayscale.lightGray2};
+  }
+`;
+
+export const Name = styled.h3`
+  font: ${({ theme }) => theme.fonts.heading3};
+  color: ${({ theme }) => theme.colors.grayscale.black};
+  text-align: center;
+  margin-top: 24px;
+`;
+
+export const Description = styled.h3`
+  font: ${({ theme }) => theme.fonts.body2};
+  color: ${({ theme }) => theme.colors.grayscale.gray2};
+  text-align: center;
+  margin-top: 8px;
+`;
+
+export const ButtonInner = styled.div`
+  display: flex;
+  justify-content: center;
 `;
