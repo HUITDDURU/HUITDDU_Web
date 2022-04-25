a52cb95fa194bf82f1d1470414664459d726efb4
김진근 <freedom7113@gmail.com>
2022-04-25 23:08:17 +0900

    🔌 (#8) - use componenets in main container

--- src/containers/Main/index.tsx
+++ src/containers/Main/index.tsx
@@ -1,5 +1,19 @@
+import DiaryHint from "../../components/DiaryHint";
+import DiaryMap from "../../components/DiaryMap";
+import Status from "../../components/Status";
+import * as S from "./styles";
+
 const MainContainer = () => {
-  return <div></div>;
+  return (
+    <S.Container>
+      <S.Title>진행중인 일기</S.Title>
+      <S.ContentContainer>
+        <DiaryMap />
+        <Status />
+        <DiaryHint />
+      </S.ContentContainer>
+    </S.Container>
+  );
 };
 
 export default MainContainer;
--- src/containers/Main/styles.ts
+++ src/containers/Main/styles.ts
@@ -1,1 +1,19 @@
-export {};
+import styled from "@emotion/styled";
+
+export const Title = styled.div`
+  font: ${({ theme }) => theme.fonts.heading1};
+  margin-bottom: 80px;
+`;
+
+export const Container = styled.div`
+  padding-top: 80px;
+  flex: 1;
+`;
+
+export const ContentContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-template-rows: repeat(2, 1fr);
+  gap: 20px;
+  flex-direction: row-reverse;
+`;
