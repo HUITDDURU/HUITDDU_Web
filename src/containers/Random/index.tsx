a8978530a9e46376780554e93e4b8dd8f5546f0d
김진근 <freedom7113@gmail.com>
2022-04-25 22:51:57 +0900

    🔌 (#8) - kill gsap on cleanup

--- src/containers/Random/index.tsx
+++ src/containers/Random/index.tsx
@@ -5,37 +5,40 @@ import * as S from "./styles";
 
 const RandomContainer = () => {
   const backdropRef = useRef<HTMLDivElement>(null);
-  const [dot, setDot] = useState(0);
+  const [dot, setDot] = useState<number>(0);
+  const findAnimationRef = useRef<gsap.core.Tween | null>(null);
 
   const backdropAnimation = useCallback(() => {
     if (!backdropRef.current) {
       return;
     }
 
-    const gsapInstance = gsap.fromTo(
+    findAnimationRef.current = gsap.fromTo(
       backdropRef.current,
       { opacity: 0.5, scale: 1 },
       { opacity: 0, scale: 3, ease: Power4.easeOut, duration: 4, repeat: -1 }
     );
-
-    return () => {
-      gsapInstance.kill();
-    };
   }, []);
 
   const nextDot = useCallback(() => {
-    setDot((prev) => (prev + 1) % 3);
+    setDot((prev) => (prev + 1) % 4);
     setTimeout(nextDot, 1000);
   }, []);
 
   useEffect(() => {
     setTimeout(backdropAnimation, 500);
     setTimeout(nextDot, 1000);
+
+    return () => {
+      if (findAnimationRef.current) {
+        findAnimationRef.current.kill();
+      }
+    };
   }, [backdropAnimation, nextDot]);
 
   return (
     <S.Container className={ANIMATED_CLASS}>
-      <S.Title>무작위 사용자 찾는중{".".repeat(dot + 1)}</S.Title>
+      <S.Title>무작위 사용자 찾는중{".".repeat(dot)}</S.Title>
       <S.ImageContainer>
         <S.Backdrop ref={backdropRef} />
         <S.ProfileImage />
