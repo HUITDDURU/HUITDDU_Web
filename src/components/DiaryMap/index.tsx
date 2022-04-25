b822de875520146f2da44d26c959919c8a262ec7
김진근 <freedom7113@gmail.com>
2022-04-25 23:07:43 +0900

    🔌 (#8) - add diary map component

---
+++ src/components/DiaryMap/index.tsx
@@ -0,0 +1,7 @@
+import * as S from "./styles";
+
+const DiaryMap = () => {
+  return <S.Container>this is diary map</S.Container>;
+};
+
+export default DiaryMap;
---
+++ src/components/DiaryMap/styles.ts
@@ -0,0 +1,6 @@
+import styled from "@emotion/styled";
+
+export const Container = styled.div`
+  grid-row: 1 / 3;
+  grid-column: 2 / 3;
+`;
