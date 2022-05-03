663f58e004bab87651aa5d63c9e6e072e4a35e6f
김진근 <freedom7113@gmail.com>
2022-05-03 17:05:42 +0900

    🔌 (#11) - publishing diary ui

--- src/containers/Diary/index.tsx
+++ src/containers/Diary/index.tsx
@@ -1,5 +1,18 @@
+import DiaryPage from "../../components/DiaryPage";
+import * as S from "./styles";
+
 const DirayContainer = () => {
-  return <div></div>;
+  return (
+    <S.Container>
+      <S.Title>user1234와의 일기장</S.Title>
+      <S.Add>+ 일기 쓰기</S.Add>
+      {Array(2)
+        .fill(0)
+        .map((_, index) => (
+          <DiaryPage key={index} />
+        ))}
+    </S.Container>
+  );
 };
 
 export default DirayContainer;
--- src/containers/Diary/styles.ts
+++ src/containers/Diary/styles.ts
@@ -1,1 +1,30 @@
-export {};
+import styled from "@emotion/styled";
+
+export const Title = styled.h1`
+  font: ${({ theme }) => theme.fonts.heading1};
+  color: ${({ theme }) => theme.colors.grayscale.black};
+  margin-bottom: 80px;
+  padding-top: 80px;
+`;
+
+export const Add = styled.button`
+  background-color: transparent;
+  padding: 28px;
+  text-align: center;
+  width: 100%;
+  border-radius: 10px;
+  border: 1px solid ${({ theme }) => theme.colors.grayscale.lightGray1};
+  transition: background-color 0.2s ease-in-out;
+  will-change: background-color;
+  width: 100%;
+  color: ${({ theme }) => theme.colors.grayscale.gray2};
+
+  &:hover,
+  &:focus-visible {
+    background-color: ${({ theme }) => theme.colors.grayscale.lightGray2};
+  }
+`;
+
+export const Container = styled.div`
+  width: 100%;
+`;
