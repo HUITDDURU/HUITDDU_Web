55cf3bbd79d217d0ea3621aa08aed5f356f585a8
김진근 <freedom7113@gmail.com>
2022-05-03 21:47:49 +0900

    🔌 - publishing write diary ui

--- src/containers/WriteDiary/index.tsx
+++ src/containers/WriteDiary/index.tsx
@@ -1,5 +1,137 @@
+import { useMemo, useState } from "react";
+import BorderBrandButton from "../../components/Buttons/BorderBrandButton";
+import Button from "../../components/Buttons/Button";
+import Editor from "../../components/Editor";
+import * as S from "./styles";
+
 const WriteDiaryContainer = () => {
-  return <div></div>;
+  const [dates] = useState([
+    {
+      value: "today",
+      label: "오늘",
+      checked: true,
+    },
+    {
+      value: "yesterday",
+      label: "어제",
+      checked: false,
+    },
+  ]);
+
+  const [feelings] = useState([
+    {
+      value: "😆",
+      checked: true,
+    },
+    {
+      value: "😭",
+      checked: false,
+    },
+    {
+      value: "🤬",
+      checked: false,
+    },
+    {
+      value: "😖",
+      checked: false,
+    },
+    {
+      value: "🤔",
+      checked: false,
+    },
+    {
+      value: "🥳",
+      checked: false,
+    },
+    {
+      value: "🤩",
+      checked: false,
+    },
+    {
+      value: "😘",
+      checked: false,
+    },
+    {
+      value: "😐",
+      checked: false,
+    },
+    {
+      value: "😣",
+      checked: false,
+    },
+  ]);
+
+  const renderDates = useMemo(
+    () =>
+      dates.map((value) => (
+        <div key={value.value}>
+          <S.DateRadioInput
+            type="radio"
+            name="date"
+            value={value.value}
+            id={value.value}
+            checked={value.checked}
+          />
+          <S.DateRadioLabel htmlFor={value.value}>
+            {value.label}
+          </S.DateRadioLabel>
+        </div>
+      )),
+    [dates]
+  );
+
+  const renderFeelings = useMemo(
+    () =>
+      feelings.map((value) => (
+        <div key={value.value}>
+          <S.FeelingRadioInput
+            type="radio"
+            name="feeling"
+            value={value.value}
+            id={value.value}
+            checked={value.checked}
+          />
+          <S.FeelingRadioLabel htmlFor={value.value}>
+            {value.value}
+          </S.FeelingRadioLabel>
+        </div>
+      )),
+    [feelings]
+  );
+
+  return (
+    <S.Container>
+      <S.Title>일기 쓰기</S.Title>
+      <S.ContentContainer>
+        <div>
+          <S.Subtitle>제목</S.Subtitle>
+          <S.TitleInput placeholder="제목을 입력해주세요..." maxLength={100} />
+        </div>
+        <div>
+          <S.Subtitle>날짜</S.Subtitle>
+          <S.DateOuter>
+            <S.DateContainer>{renderDates}</S.DateContainer>
+            <span>선택된 날짜 : 2022년 1월 1일</span>
+          </S.DateOuter>
+        </div>
+        <div>
+          <S.Subtitle>기분</S.Subtitle>
+          <S.FeelingContainer>{renderFeelings}</S.FeelingContainer>
+        </div>
+        <div>
+          <S.Subtitle>사진</S.Subtitle>
+          <BorderBrandButton>사진 업로드</BorderBrandButton>
+        </div>
+        <div>
+          <S.Subtitle>내용</S.Subtitle>
+          <Editor />
+        </div>
+      </S.ContentContainer>
+      <S.Buttons>
+        <Button>교환</Button>
+      </S.Buttons>
+    </S.Container>
+  );
 };
 
 export default WriteDiaryContainer;
--- src/containers/WriteDiary/styles.ts
+++ src/containers/WriteDiary/styles.ts
@@ -1,1 +1,93 @@
-export {};
+import styled from "@emotion/styled";
+import Input from "../../components/Input";
+
+export const Title = styled.div`
+  font: ${({ theme }) => theme.fonts.heading1};
+  margin: 80px 0px;
+  color: ${({ theme }) => theme.colors.grayscale.black};
+`;
+
+export const Container = styled.div`
+  width: 100%;
+  margin-bottom: 200px;
+`;
+
+export const Subtitle = styled.div`
+  font: ${({ theme }) => theme.fonts.body2};
+  color: ${({ theme }) => theme.colors.grayscale.gray1};
+  margin-bottom: 8px;
+`;
+
+export const TitleInput = styled(Input)`
+  font: ${({ theme }) => theme.fonts.heading3};
+  color: ${({ theme }) => theme.colors.grayscale.black};
+
+  &::placeholder {
+    color: ${({ theme }) => theme.colors.grayscale.gray2};
+    font: ${({ theme }) => theme.fonts.heading3};
+  }
+`;
+
+export const ContentContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  row-gap: 28px;
+`;
+
+export const DateContainer = styled.div`
+  display: flex;
+  column-gap: 16px;
+`;
+
+export const DateOuter = styled.div`
+  display: flex;
+  column-gap: 40px;
+  font: ${({ theme }) => theme.fonts.body2};
+  color: ${({ theme }) => theme.colors.grayscale.gray2};
+`;
+
+export const DateRadioInput = styled.input`
+  display: none;
+  &:checked + label {
+    color: ${({ theme }) => theme.colors.primary};
+  }
+`;
+
+export const DateRadioLabel = styled.label`
+  transition: color 0.2s ease-in-out;
+  font: ${({ theme }) => theme.fonts.body2};
+  color: ${({ theme }) => theme.colors.grayscale.gray2};
+  user-select: none;
+`;
+
+export const FeelingContainer = styled(DateContainer)`
+  column-gap: 12px;
+`;
+
+export const FeelingRadioInput = styled(DateRadioInput)`
+  &:checked + label {
+    color: ${({ theme }) => theme.colors.grayscale.black};
+    background-color: ${({ theme }) => theme.colors.primary};
+  }
+`;
+
+export const FeelingRadioLabel = styled.label`
+  transition: background-color 0.2s ease-in-out;
+  font: ${({ theme }) => theme.fonts.body2};
+  color: ${({ theme }) => theme.colors.grayscale.black};
+  user-select: none;
+  background-color: ${({ theme }) => theme.colors.grayscale.lightGray2};
+  width: 30px;
+  height: 30px;
+  display: block;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 10px;
+`;
+
+export const Buttons = styled.div`
+  display: flex;
+  justify-content: right;
+  margin-top: 16px;
+`;
