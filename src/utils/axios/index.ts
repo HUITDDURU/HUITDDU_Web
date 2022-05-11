c03e0239f9e8a0502c89303711c3d8831611b777
김진근 <freedom7113@gmail.com>
2022-05-11 15:14:22 +0900

    🔌 (#7) - implement refresh token logic

--- src/utils/axios/index.ts
+++ src/utils/axios/index.ts
@@ -1,9 +1,12 @@
 import axios from "axios";
-import baseURL from "../constant/baseURL";
+import baseURL from "../../constant/baseURL";
+import { saveTokenInReqestHeader } from "./interceptors";
 
 const request = axios.create({ baseURL: baseURL });
 const requestWithNoToken = axios.create({
   baseURL: baseURL,
 });
+
+request.interceptors.request.use(saveTokenInReqestHeader);
 
 export { request, requestWithNoToken };
