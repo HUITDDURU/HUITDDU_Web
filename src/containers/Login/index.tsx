9eb4a281121b3fe72f93aca4e41f86cc7a86c2cf
김진근 <freedom7113@gmail.com>
2022-05-14 23:15:37 +0900

    🔌 (#7) - use form input in login

--- src/containers/Login/index.tsx
+++ src/containers/Login/index.tsx
@@ -6,7 +6,7 @@ import React, { memo, useMemo, useRef, useState } from "react";
 import toast from "react-hot-toast";
 import BorderButton from "../../components/Buttons/BorderButton";
 import Button from "../../components/Buttons/Button";
-import Input from "../../components/Input";
+import FormInput, { FormInputRef } from "../../components/FormInput";
 import storageKeys from "../../constant/storageKeys";
 import { useLogin } from "../../queries/Auth";
 import * as S from "./styles";
@@ -14,6 +14,10 @@ import * as S from "./styles";
 interface LoginInput {
   email: string;
   password: string;
+}
+interface ErrorMessages {
+  email?: string;
+  password?: string;
 }
 
 const LoginContainer: NextPage = () => {
@@ -29,8 +33,8 @@ const LoginContainer: NextPage = () => {
       loginMutation.isLoading,
     [email, loginMutation.isLoading, password]
   );
-  const emailRef = useRef<HTMLInputElement>(null);
-  const passwordRef = useRef<HTMLInputElement>(null);
+  const emailRef = useRef<FormInputRef>(null);
+  const passwordRef = useRef<FormInputRef>(null);
 
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
@@ -46,15 +50,30 @@ const LoginContainer: NextPage = () => {
     return emailRegex.test(email);
   };
 
+  const setError = ({ email, password }: ErrorMessages) => {
+    if (email) {
+      emailRef.current?.error(email);
+    } else {
+      emailRef.current?.error("");
+    }
+
+    if (password) {
+      passwordRef.current?.error(password);
+    } else {
+      passwordRef.current?.error("");
+    }
+  };
+
   const onLoginClick = async () => {
     if (!checkValidity()) {
-      toast.error("이메일 형식을 확인해주세요.");
+      setError({ email: "유효하지 않은 이메일입니다." });
       setLoginInput({ password: "", email: "" });
-      emailRef.current?.focus();
+      emailRef.current?.ref.current?.focus();
       return;
     }
 
     try {
+      setError({});
       const { data } = await loginMutation.mutateAsync({ email, password });
       const { accessToken, refreshToken } = data;
       const expiresAt = moment().add(30, "minute");
@@ -67,7 +86,7 @@ const LoginContainer: NextPage = () => {
       if (axios.isAxiosError(error) && error.response?.status === 404) {
         toast.error("로그인 실패. 다시 확인해주세요.");
         setLoginInput((prev) => ({ ...prev, password: "" }));
-        passwordRef.current?.focus();
+        passwordRef.current?.ref.current?.focus();
         return;
       }
 
@@ -87,31 +106,27 @@ const LoginContainer: NextPage = () => {
     <div>
       <S.Title>로그인</S.Title>
       <S.InputContainer>
-        <div>
-          <S.Description>이메일</S.Description>
-          <Input
-            placeholder="이메일을 입력해주세요..."
-            value={email}
-            name="email"
-            autoComplete="username"
-            type="email"
-            onChange={onChange}
-            ref={emailRef}
-          />
-        </div>
-        <div>
-          <S.Description>비밀번호</S.Description>
-          <Input
-            placeholder="비밀번호를 입력해주세요..."
-            value={password}
-            name="password"
-            type="password"
-            autoComplete="current-password"
-            onChange={onChange}
-            ref={passwordRef}
-            onKeyDown={onKeyDown}
-          />
-        </div>
+        <FormInput
+          title="이메일"
+          placeholder="이메일을 입력해주세요..."
+          value={email}
+          name="email"
+          autoComplete="username"
+          type="email"
+          onChange={onChange}
+          ref={emailRef}
+        />
+        <FormInput
+          title="비밀번호"
+          placeholder="비밀번호를 입력해주세요..."
+          value={password}
+          name="password"
+          type="password"
+          autoComplete="current-password"
+          onChange={onChange}
+          ref={passwordRef}
+          onKeyDown={onKeyDown}
+        />
       </S.InputContainer>
       <S.ButtonContainer>
         <BorderButton onClick={onSignUpClick}>회원가입</BorderButton>
