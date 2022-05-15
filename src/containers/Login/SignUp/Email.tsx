449ffd2d340511182cd2a023a558ed3b0198acba
김진근 <freedom7113@gmail.com>
2022-05-15 11:15:07 +0900

    🔌 (#7) - check email code on next in email

--- src/containers/Login/SignUp/Email.tsx
+++ src/containers/Login/SignUp/Email.tsx
@@ -1,12 +1,13 @@
 import { NextPage } from "next";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import React, { memo, useCallback, useEffect } from "react";
+import React, { memo, useCallback, useEffect, useRef } from "react";
 import { useRecoilState } from "recoil";
 import signUpState from "../../../atom/signUpState";
 import Button from "../../../components/Buttons/Button";
-import FormInput from "../../../components/FormInput";
+import FormInput, { FormInputRef } from "../../../components/FormInput";
 import useRouterWithPageTransition from "../../../hooks/useRouterWithPageTransition";
+import { useEmail } from "../../../queries/Auth";
 import * as S from "../styles";
 
 const EmailContainer: NextPage = () => {
@@ -14,17 +15,39 @@ const EmailContainer: NextPage = () => {
   const [signUp, setSignUp] = useRecoilState(signUpState);
   const { email, certificationCode, isEmailConfirmationed } = signUp;
   const push = useRouterWithPageTransition();
+  const { certificationCode: codeCertification } = useEmail();
+  const codeRef = useRef<FormInputRef>(null);
 
   const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) =>
     setSignUp((prev) => ({ ...prev, certificationCode: e.target.value }));
 
-  const isNextDisabled = certificationCode.length <= 0;
+  const isNextDisabled =
+    certificationCode.length <= 0 || codeCertification.isLoading;
 
   const checkEmail = useCallback(async () => {
     if (email === "") {
-      push("enterinfo", 500);
+      await push("enterinfo", 500);
     }
-  }, [email, push]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [email]);
+
+  const onNextClick = async () => {
+    if (certificationCode.trim() === "") {
+      codeRef.current?.error("인증코드를 입력해주세요.");
+      return;
+    }
+
+    codeRef.current?.error("");
+    try {
+      await codeCertification.mutateAsync({ email, code: certificationCode });
+      await router.push("profile");
+      setSignUp((prev) => ({ ...prev, isEmailConfirmationed: true }));
+    } catch (error) {
+      codeRef.current?.error("인증코드가 틀렸습니다. 다시 확인해주세요.");
+      codeRef.current?.ref.current?.focus();
+      setSignUp((prev) => ({ ...prev, certificationCode: "" }));
+    }
+  };
 
   useEffect(() => {
     checkEmail();
@@ -35,6 +58,7 @@ const EmailContainer: NextPage = () => {
       <S.Title>이메일 인증</S.Title>
       <S.InputContainer>
         <FormInput
+          ref={codeRef}
           title="이메일 인증 코드"
           value={certificationCode}
           disabled={isEmailConfirmationed}
@@ -51,11 +75,8 @@ const EmailContainer: NextPage = () => {
           <S.ToLogin>
             <Link href="/login/signup/enterinfo">이전</Link>
           </S.ToLogin>
-          <Button
-            disabled={isNextDisabled}
-            onClick={() => router.push("profile")}
-          >
-            다음
+          <Button disabled={isNextDisabled} onClick={onNextClick}>
+            {isEmailConfirmationed ? "다음" : "인증"}
           </Button>
         </S.ButtonContainer>
       </S.BottomContainer>
