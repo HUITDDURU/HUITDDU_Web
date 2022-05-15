515f1140553d64230fce58e0b9083c6a47413a1a
김진근 <freedom7113@gmail.com>
2022-05-15 10:44:23 +0900

    🔌 (#7) - memorize in use router with page transition

--- src/hooks/useRouterWithPageTransition.ts
+++ src/hooks/useRouterWithPageTransition.ts
@@ -1,28 +1,32 @@
 import { Mutex } from "async-mutex";
 import { useRouter } from "next/router";
+import { useCallback } from "react";
 
 const mutex = new Mutex();
 
 const useRouterWithPageTransition = () => {
   const router = useRouter();
 
-  const push = async (url: string, delay: number) => {
-    if (!mutex.isLocked()) {
-      const release = await mutex.acquire();
+  const push = useCallback(
+    async (url: string, delay: number) => {
+      if (!mutex.isLocked()) {
+        const release = await mutex.acquire();
 
-      try {
-        await router.push(url);
-        await new Promise<void>((resolve) => {
-          setTimeout(() => {
-            release();
-            resolve();
-          }, delay);
-        });
-      } finally {
-        release();
+        try {
+          await router.push(url);
+          await new Promise<void>((resolve) => {
+            setTimeout(() => {
+              release();
+              resolve();
+            }, delay);
+          });
+        } finally {
+          release();
+        }
       }
-    }
-  };
+    },
+    [router]
+  );
 
   return push;
 };
