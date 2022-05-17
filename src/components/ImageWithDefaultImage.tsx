e4c28ff05eef1a8b6aa8396f8338d3a9cccce569
김진근 <freedom7113@gmail.com>
2022-05-17 14:14:31 +0900

    🔌 (#15) - add image with default image component

---
+++ src/components/ImageWithDefaultImage.tsx
@@ -0,0 +1,26 @@
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import Image, { ImageProps } from "next/image";
+import { FC, useState } from "react";
+
+interface PropsType {
+  defaultImage: StaticImageData;
+  src: string | StaticImageData | null;
+}
+
+const ImageWithDefaultImage: FC<Omit<ImageProps, "src"> & PropsType> = (
+  props
+) => {
+  const { src, defaultImage, ...rest } = props;
+  const [srcState, setSrcState] = useState<typeof src>(src);
+
+  const onImageError = () => {
+    setSrcState(defaultImage);
+  };
+
+  return (
+    <Image onError={onImageError} {...rest} src={srcState || defaultImage} />
+  );
+};
+
+export default ImageWithDefaultImage;
