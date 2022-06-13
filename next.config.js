3315692536e8b39c36f536673f3a3b65dcaeac5d
김진근 <freedom7113@gmail.com>
2022-06-13 19:26:55 +0900

    🔌 (#22) - add env in config

--- next.config.js
+++ next.config.js
@@ -4,6 +4,10 @@ const nextConfig = {
   images: {
     domains: ["huitdduru.s3.ap-northeast-2.amazonaws.com"],
   },
+  env: {
+    NEXT_PUBLIC_BASEURL: process.env.NEXT_PUBLIC_BASEURL,
+    NEXT_PUBLIC_SOCKET_BASEURL: process.env.NEXT_PUBLIC_SOCKET_BASEURL,
+  },
 };
 
 module.exports = nextConfig;
