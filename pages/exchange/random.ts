f6590adac24b7bc92e775bfe66bfb498bf42b0df
김진근 <freedom7113@gmail.com>
2022-06-14 14:06:50 +0900

    🔌 (#22) - connect socket in random matching

--- pages/exchange/random.ts
+++ pages/exchange/random.ts
@@ -1,1 +1,24 @@
+import { GetServerSideProps } from "next";
+import cookies from "next-cookies";
+import { dehydrate, QueryClient } from "react-query";
+import { getProfile } from "../../src/api/My";
+import queryKeys from "../../src/constant/queryKeys";
+import storageKeys from "../../src/constant/storageKeys";
+import { request } from "../../src/utils/axios";
+
 export { RandomContainer as default } from "../../src/containers";
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const allCookies = cookies(ctx);
+  const accessToken = allCookies[storageKeys.accessToken];
+  request.defaults.headers.common["Authorization"] = `Bearer ${accessToken}`;
+
+  const queryClient = new QueryClient();
+  await queryClient.prefetchQuery([queryKeys.my], getProfile);
+
+  return {
+    props: {
+      dehydratedState: dehydrate(queryClient),
+    },
+  };
+};
---
+++ src/@types/socketResponse.ts
@@ -0,0 +1,5 @@
+export interface MatchingUser {
+  name: string;
+  img: string | null;
+  intro: string | null;
+}
